fc069008b1da37c5d15191f7f7b23a3c1a067b89
jacksontenorio8 <jacksontenorio8@gmail.com>
2021-09-19 21:24:02 -0300

    Update

--- src/main/java/br/com/jacksontenorio8/cursopoo/aula05/ContaBanco.java
+++ src/main/java/br/com/jacksontenorio8/cursopoo/aula05/ContaBanco.java
@@ -7,6 +7,15 @@ public class ContaBanco {
     private String dono;
     private float saldo;
     private boolean status;
+    public void estadoAtual(){
+        System.out.println("----------------------------");
+        System.out.println("Conta: " + this.getNumConta());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("Status: " + this.getStatus());
+    }
+
 
     public void abrirConta(String t){
         this.setTipo(t);
@@ -37,10 +46,30 @@ public class ContaBanco {
         }
     }
     public void sacar(float v){
-
+        if (this.getStatus()){
+            if (this.getSaldo() >= v) {
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("Saque realizado na conta "+ getDono());
+            } else {
+                System.out.println("Saldo insuficiente para saque.");
+            }
+        } else {
+            System.out.println("Impossível sacar de uma conta fechada.");
+        }
     }
     public void pagarMensal(){
-
+        int v = 0;
+        if (this.getTipo() == "CC"){
+            v = 12;
+        }else if (this.getTipo() == "CP"){
+            v = 20;
+        }
+        if (this.getStatus()) {
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Mensalidade paga com sucesso por "+ this.getDono());
+        } else {
+            System.out.println("Impossível pagar uma conta fechada!");
+        }
     }
     // Métodos Especiais
 
--- src/main/java/br/com/jacksontenorio8/cursopoo/aula05/aula05.java
+++ src/main/java/br/com/jacksontenorio8/cursopoo/aula05/aula05.java
@@ -1,4 +1,14 @@
 package br.com.jacksontenorio8.cursopoo.aula05;
 
 public class aula05 {
+    public static void main(String[] args) {
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(1111);
+        p1.setDono("Jackson");
+        p1.abrirConta("CC");
+        p1.estadoAtual();
+
+        ContaBanco p2 = new ContaBanco();
+        p2.setNumConta(2222);
+    }
 }
