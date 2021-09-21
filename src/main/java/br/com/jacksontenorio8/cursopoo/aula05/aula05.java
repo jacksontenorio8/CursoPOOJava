4dc73310297f238b8fc87fb98d36614250b4a298
jacksontenorio8 <jacksontenorio8@gmail.com>
2021-09-21 16:42:49 -0300

    Update

--- src/main/java/br/com/jacksontenorio8/cursopoo/aula05/aula05.java
+++ src/main/java/br/com/jacksontenorio8/cursopoo/aula05/aula05.java
@@ -6,9 +6,21 @@ public class aula05 {
         p1.setNumConta(1111);
         p1.setDono("Jackson");
         p1.abrirConta("CC");
-        p1.estadoAtual();
+
 
         ContaBanco p2 = new ContaBanco();
         p2.setNumConta(2222);
+        p2.setDono("Daysy anne");
+        p2.abrirConta("CP");
+
+        p1.depositar(100);
+        p2.depositar(500);
+        p2.sacar(1000);
+
+        p1.sacar(150);
+        p1.fecharConta();
+
+        p1.estadoAtual();
+        p2.estadoAtual();
     }
 }
